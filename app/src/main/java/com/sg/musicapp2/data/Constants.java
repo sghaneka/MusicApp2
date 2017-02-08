df700044239ac268ded8f4647e085c5b7fd64fec
sam g <sghaneka@yahoo.com>
2017-02-08 17:29:24 -0500

    yeah - playlists finally getting

---
+++ app/src/main/java/com/sg/musicapp2/data/Constants.java
@@ -0,0 +1,13 @@
+package com.sg.musicapp2.data;
+
+/**
+ * Created by samgh on 2/8/2017.
+ */
+
+public class Constants {
+    public static final String LIMIT = "limit";
+    public static final String OFFSET = "offset";
+    public static final String TRACK_ID = "trackId";
+    public static final String APIKEY = "apikey";
+    public static final String AUTHORIZAION = "Authorization";
+}
