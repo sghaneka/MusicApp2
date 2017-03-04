e94e7d2541b2dd40fe11cdcc20def17d3c309b63
Sam Ghanekar <sghaneka@yahoo.com>
2017-03-04 13:15:15 -0500

    first cut of bringing in that player.... .

--- app/src/main/java/com/sg/musicapp2/MusicApplication.java
+++ app/src/main/java/com/sg/musicapp2/MusicApplication.java
@@ -38,6 +38,10 @@ public class MusicApplication extends Application {
         return napster;
     }
 
+    public Player getPlayer() {
+        return player;
+    }
+
     public SessionManager getSessionManager() {
         return sessionManager;
     }
